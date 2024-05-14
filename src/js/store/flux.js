1065945ea93367f6de637cda5b537aa80f0c9aae
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-05-14 16:23:19 +0000

    commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,100 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			//Your data structures, A.K.A Entities
+			user: "",
+			agenda: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			// (Arrow) Functions that update the Store
+			// Remember to use the scope: scope.state.store & scope.setState()
+			getUser: () => {
+				const requestOptions = { method: "GET", redirect: "follow" };
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/", requestOptions)
+					.then(response => response.json())
+					.then(result => console.log(result))
+					.catch(error => console.error(error));
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			createUser: contactData => {
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				const raw = JSON.stringify(contactData);
+
+				const requestOptions = {
+					method: "POST",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/", requestOptions)
+					.then(response => response.json())
+					.then(result => console.log(result))
+					.catch(error => console.error(error));
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getAgenda: () => {
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/natalial.salas", {
+					method: "GET",
+					redirect: "follow"
+				})
+					.then(response => response.json())
+					.then(result => {
+						setStore({ agenda: result });
+					})
+					.catch(error => console.error(error));
+			},
+			deleteContact: (contactId, contact) => {
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				const raw = JSON.stringify(contact);
 
-				//reset the global store
-				setStore({ demo: demo });
+				const requestOptions = {
+					method: "DELETE",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				fetch(`https://playground.4geeks.com/apis/fake/contact/${contactId}`, requestOptions)
+					.then(response => {
+						response.json();
+						if (response.status == 201) {
+							getActions().getAgenda();
+						}
+					})
+					.then(result => {
+						console.log(result);
+					})
+					.catch(error => console.error(error));
+			},
+			updateContact: (contactId, contact) => {
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				const raw = JSON.stringify(contact);
+
+				const requestOptions = {
+					method: "PUT",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				fetch(`https://playground.4geeks.com/apis/fake/contact/${contactId}`, requestOptions)
+					.then(response => {
+						console.log(response);
+						response.json();
+						if (response.status == 201) {
+							getActions().getAgenda();
+						}
+					})
+					.then(result => {
+						console.log(result);
+						console.log("hola hola...");
+					})
+					.catch(error => console.error(error));
 			}
 		}
 	};
